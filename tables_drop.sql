e508a0f47218225e818ae1bd9da1ba256e8bba80
kennethongcs <kenneth.ongcs@gmail.com>
2022-03-31 23:39:42 +0800

    add logic to drop all tables

---
+++ tables_drop.sql
@@ -0,0 +1,13 @@
+DROP TABLE accounts;
+DROP TABLE customers;
+DROP TABLE loadings;
+DROP TABLE loading_containers;
+DROP TABLE container_sizes;
+DROP TABLE container_types;
+DROP TABLE vessel_voyage;
+DROP TABLE vessel_schedule;
+DROP TABLE vessel_name;
+DROP TABLE country;
+DROP TABLE service_name;
+DROP TABLE port_name;
+DROP TABLE vessel_alloc_at_port;
